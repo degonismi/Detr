15b6796c54741b3a9b813bdc3f5c2447cc9401ca
Denis-Kovalenak <49638569+Denis-Kovalenak@users.noreply.github.com>
2019-06-26 18:02:23 +0300

    add cameranew input

--- Assets/Scripts/Cube_Detector.cs
+++ Assets/Scripts/Cube_Detector.cs
@@ -7,52 +7,52 @@ public class Cube_Detector : MonoBehaviour
     public Material Cube_touched;
     GameObject GG,sph;
     bool check,lkm;
-    // Start is called before the first frame update
+
     void Start()
     {
-        GG = GameObject.FindGameObjectWithTag("Player");
-        sph = GameObject.FindGameObjectWithTag("EditorOnly");
+        //GG = GameObject.FindObjectOfType<Moving>().gameObject;
+        //sph = GameObject.FindGameObjectWithTag("PlayerSphere");
         check = false;
-        lkm = false;
+       // lkm = false;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (GG != null)
-        {
-            if (Vector3.Distance(GG.transform.position, transform.position) < 1f)
-            {
-                Change_mat();
-                Init();
-            }
-        }
-    }
 
-    public void Init()
-    {
-        /*if (Vector3.Distance(GG.transform.position, transform.position) > 1 && !check && Input.GetMouseButtonDown(0))
-        {
-            lkm = true;
-        }
-        if (Vector3.Distance(GG.transform.position, transform.position) <= 1 && !check)
-        {
-            Change_mat();
-            if (lkm)
-            {
-                Moving.mov_left = !Moving.mov_left;
-                gameObject.GetComponent<Cube_Detector>().enabled = false;
-            }
-        }*/
-        if (GG.transform.position.y- transform.position.y > 0.5f&& Input.GetMouseButtonDown(0))
-        {
-            Moving.mov_left = !Moving.mov_left;
-            GG.transform.position = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
-            sph.SetActive(false);
-            sph.SetActive(true);
-        }
+    //void Update()
+    //{
+    //    //if (GG != null)
+    //    //{
+    //     //   if (Vector3.Distance(GG.transform.position, transform.position) < 1f)
+    //    //    {
+    //      //      Change_mat();
+    //     //Init();
+    //     //   }
+    //   // }
+    //}
+
+    //public void Init()
+    //{
+    //    /*if (Vector3.Distance(GG.transform.position, transform.position) > 1 && !check && Input.GetMouseButtonDown(0))
+    //    {
+    //        lkm = true;
+    //    }
+    //    if (Vector3.Distance(GG.transform.position, transform.position) <= 1 && !check)
+    //    {
+    //        Change_mat();
+    //        if (lkm)
+    //        {
+    //            Moving.mov_left = !Moving.mov_left;
+    //            gameObject.GetComponent<Cube_Detector>().enabled = false;
+    //        }
+    //    }*/
+    //    if (GG.transform.position.y- transform.position.y > 0.5f&& Input.GetMouseButtonDown(0))
+    //    {
+    //        Moving.mov_left = !Moving.mov_left;
+    //        GG.transform.position = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
+    //        sph.SetActive(false);
+    //        sph.SetActive(true);
+    //    }
        
-    }
+    //}
     
     public void Change_mat()
     {
--- Assets/Scripts/Moving.cs
+++ Assets/Scripts/Moving.cs
@@ -9,36 +9,58 @@ public class Moving : MonoBehaviour
     public GameObject sphere,part,camera;
     float start_speed, speed_fail;
 
+    public bool _changeDirection;
+
+    private int _prevPos;
+    private float _curPos;
+
+    private Vector3 _pos;
+
     void Start()
     {
-        mov_left = true;
+        mov_left = false;
         dead = false;
         start = false;
         speed_fail = 0;
         sphere.GetComponent<Animator>().enabled = false;
+        _prevPos = (int) transform.position.y;
+        _changeDirection = false;
     }
     void Update()
     {
+        camera.transform.position = Vector3.Lerp(camera.transform.position, new Vector3(transform.position.x, transform.position.y - 3.5f, transform.position.z), 1 * Time.deltaTime);
+        // if (Input.GetMouseButtonDown(0)) ChangeDirection();
         if (start) {
             sphere.GetComponent<Animator>().enabled = true;
             start_speed = speed;
-            if (dead)
-            {
-                Dead_Init();
-            }
-            else
-            {
-                Move_Init();
-            }
+            Move_Init();
+            //if (dead)
+            //{
+            //    Dead_Init();
+            //}
+            //else
+            //{
+            //    Move_Init();
+            //}
         }
         else
         {
             start_speed = 0;
         }
+
+        if (transform.position.y <= _prevPos-1)
+        {
+            _prevPos = (int)transform.position.y;
+            mov_left = _changeDirection;
+        }
+
+        
+
     }
+    
     public void Move_Init()
     {
-        camera.transform.position = Vector3.Lerp(camera.transform.position, new Vector3(transform.position.x, transform.position.y - 3.5f, transform.position.z),1*Time.deltaTime) ;
+        
         if (mov_left)
         {
             transform.Translate(-speed * Time.deltaTime, -speed * Time.deltaTime, 0);
@@ -53,9 +75,29 @@ public class Moving : MonoBehaviour
         UI.retry_active = true;
         sphere.SetActive(false);
         part.SetActive(true);
-        gameObject.GetComponent<MeshRenderer>().enabled = true;
+        //gameObject.GetComponent<MeshRenderer>().enabled = true;
         speed_fail = Mathf.Lerp(speed_fail, -40, 4 * Time.deltaTime);
         transform.Translate(0, speed_fail * Time.deltaTime, 0);
         Destroy(gameObject, 1);
     }
+
+
+    public void ChangeDirection()
+    {
+        _changeDirection = !_changeDirection;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Cube_Detector>())
+        {
+            other.GetComponent<Cube_Detector>().Change_mat();
+        }
+        else if(other.GetComponent<Dead>())
+        {
+            Dead_Init();
+        }
+    }
+
+
 }
